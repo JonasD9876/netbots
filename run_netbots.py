4f24fab36bce0259f3db1b71b17210432667e667
JonasD9876 <jonasdornbusch@outlook.de>
2025-06-16 21:25:22 +0200

    changed two parameters

--- run_netbots.py
+++ run_netbots.py
@@ -49,11 +49,11 @@ def run_python_in_terminal(title, command):
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
 # Launch the processes
-run_python_in_terminal("NetBot-Server", "src/netbots_server.py -p 20000 -stepsec 0.05 -games 10 -stepmax 3000")
+run_python_in_terminal("NetBot-Server", "src/netbots_server.py -p 20000 -stepsec 0.05 -games 3 -stepmax 3000")
 run_python_in_terminal("NetBot-Viewer", "src/netbots_viewer.py -p 20001 -sp 20000")
-# run_python_in_terminal("hideincorner", "robots/hideincorner.py -p 20002 -sp 20000")
-run_python_in_terminal("circler1", "robots/circler.py -p 20002 -sp 20000")
+run_python_in_terminal("circler", "robots/circler.py -p 20002 -sp 20000")
 run_python_in_terminal("lighthouse", "robots/lighthouse.py -p 20003 -sp 20000")
 run_python_in_terminal("scaredycat", "robots/scaredycat.py -p 20004 -sp 20000")
-run_python_in_terminal("circler2", "robots/circler.py -p 20005 -sp 20000")
+# run_python_in_terminal("circler2", "robots/circler.py -p 20005 -sp 20000")
+run_python_in_terminal("hideincorner", "robots/hideincorner.py -p 20005 -sp 20000")
 # run_python_in_terminal("wallbanger", "robots/wallbanger.py -p 20005 -sp 20000")
